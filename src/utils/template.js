af637680443a05e9e58351f38e6d83c37892b28d
xiexueliang <xiexueliang@doupai.cc>
2020-11-18 14:21:19 +0800

    适配方式

--- src/utils/template.js
+++ src/utils/template.js
@@ -11,23 +11,12 @@ const tpl = () =>
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1,user-scalable=no">
-      <script>
-        (function (doc, win) {
-            var docEl = doc.documentElement,
-                resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-                recalc = function () {
-                    var clientWidth = docEl.clientWidth;
-                    if (!clientWidth) return;
-                    docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
-                };
-
-            if (!doc.addEventListener) return;
-            win.addEventListener(resizeEvt, recalc, false);
-            doc.addEventListener('DOMContentLoaded', recalc, false);
-        })(document, window);
-      </script>
       <title>${config.title || '标题'}</title>
       <style>
+        html {
+          font-size: calc(100vw/3.75);
+          -webkit-text-size-adjust: 100%;
+        }
         body {
           width: 100%;
           margin: 0;
@@ -69,3 +58,20 @@ const fixedBtn = () => {
   })
   return tpl
 }
+
+const fixable =
+`<script>
+  (function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+  })(document, window);
+</script>`
